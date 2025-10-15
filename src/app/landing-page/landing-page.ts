eeccad0ea0ebaa788ac4aafff716b04cd971b1d9
b-rich-dev <eugen.birich@gmail.com>
2025-10-15 16:05:48 +0200

    first commit: set base structure

---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../shared/header/header';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [Header],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.scss'
+})
+export class LandingPage {
+
+}
