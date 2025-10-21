424d77729bfab0e69deba392d09e09f5cc152047
b-rich-dev <eugen.birich@gmail.com>
2025-10-21 07:50:22 +0200

    feat: buttons animation only witch css

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Buttons } from './buttons/buttons';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Buttons],
+  imports: [],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss'
 })
