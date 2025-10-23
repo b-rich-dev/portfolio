04922b13ddee672cf3fc0a097f4a25da6de285e8
b-rich-dev <eugen.birich@gmail.com>
2025-10-23 13:22:45 +0200

    feat: add skill set component with template and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { Header } from './shared/header/header';
 import { AboutMe } from './about-me/about-me';
+import { SkillSet } from './skill-set/skill-set';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPage, Header, AboutMe],
+  imports: [RouterOutlet, LandingPage, Header, AboutMe, SkillSet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
