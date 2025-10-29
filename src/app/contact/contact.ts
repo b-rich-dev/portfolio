1546931dae9e63ac305eee35b640995ef2986953
b-rich-dev <eugen.birich@gmail.com>
2025-10-29 16:51:32 +0100

    feat: add warning messages for unaccepted privacy policy and adjust layout and add email send functionality

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
 
+  mailTest = true;
+
+  http = inject(HttpClient);
+
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  acceptPolicy: boolean = false;
+
+  post = {
+    endPoint: 'https://birich.it/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) { //&& this.acceptPolicy
+      console.log('Form submitted:', this.contactData);
+
+      ngForm.resetForm();
+      this.acceptPolicy = false;
+
+    }
+  }
+
+  toggleAcceptPolicy() {
+    this.acceptPolicy = !this.acceptPolicy;
+  }
 }
