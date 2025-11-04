2b6c4fa32bbf8b22f0ec927ac49745abb135aba0
b-rich-dev <eugen.birich@gmail.com>
2025-11-04 11:58:40 +0100

    style: made header responsive

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Signal } from '@angular/core';
+import { Component, inject, Signal, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LanguageService } from '../../services/language';
 
@@ -13,7 +13,38 @@ export class Header {
   currentLanguage: Signal<'en' | 'de'> = inject(LanguageService).language;
   set = inject(LanguageService);
 
+  @ViewChild('navDialog') navDialog!: ElementRef<HTMLDialogElement>;
+
   toggleLanguage() {
     this.set.toggleLanguageService();
+  }
+
+  openNavDialog(){
+    const dlg = this.navDialog?.nativeElement;
+    if (!dlg) return;
+    dlg.showModal();
+    document.body.classList.add('no-scroll');
+
+    const onBackdropClick = (e: MouseEvent) => {
+      if (e.target === dlg) {
+        dlg.close();
+      }
+    };
+
+    const onClose = () => {
+      document.body.classList.remove('no-scroll');
+      dlg.removeEventListener('click', onBackdropClick);
+      dlg.removeEventListener('close', onClose);
+    };
+
+    dlg.addEventListener('click', onBackdropClick);
+    dlg.addEventListener('close', onClose);
+  }
+
+  public closeNavDialog(): void {
+    const dlg = this.navDialog?.nativeElement;
+    if (!dlg) return;
+    dlg.close();
+    document.body.classList.remove('no-scroll');
   }
 }
