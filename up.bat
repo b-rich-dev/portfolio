dfea51d8c733263fe037817bde2a4c8462033110
b-rich-dev <eugen.birich@googlemail.com>
2026-04-13 15:14:12 +0200

    fix: update git ftp push command to include --dirty flag

--- up.bat
+++ up.bat
@@ -5,6 +5,6 @@ git push
 
 call ng build
 echo Build exit code: %ERRORLEVEL%
-git ftp push -D --syncroot dist/portfolio/browser
+git ftp push --dirty --syncroot dist/portfolio/browser
 echo FTP exit code: %ERRORLEVEL%
 pause
