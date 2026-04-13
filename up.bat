bf2a40a3a1a0ce89699d8385b6a928251b941f89
b-rich-dev <eugen.birich@googlemail.com>
2026-04-13 15:18:03 +0200

    fix: remove echo statements and --dirty flag from git ftp push command

--- up.bat
+++ up.bat
@@ -4,7 +4,4 @@ git commit -m "%*"
 git push
 
 call ng build
-echo Build exit code: %ERRORLEVEL%
-git ftp push --dirty --syncroot dist/portfolio/browser
-echo FTP exit code: %ERRORLEVEL%
-pause
+git ftp push --syncroot dist/portfolio/browser
